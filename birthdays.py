dd4f469d028e8d414aa278835f2b35485d497c82
Federico Madaro <federicomadaro@MacBook-Air-di-Federico.local>
2023-11-20 15:05:46 +0100

    Documented birtdays.py

--- birthdays.py
+++ birthdays.py
@@ -1,3 +1,6 @@
+''' 
+The goal of the module is to give the user infos about birthdays
+'''
 birthdays = {
     'Albert Einstein': '03/14/1879',
     'Benjamin Franklin': '01/17/1706',
@@ -6,11 +9,13 @@ birthdays = {
     'Rowan Atkinson': '01/6/1955'}
 
 def print_birthdays():
-    print('Welcome to the birthday dictionary. We know the birthdays of these people:')
+    ''' The function welcomes the user '''
+    print('Welcome to the birthday dictionary.')
     for name in birthdays:
         print(name)
 
 def return_birthday(name):
+    ''' The function gives the birthday date '''
     if name in birthdays:
         print('{}\'s birthday is {}.'.format(name, birthdays[name]))
     else:
