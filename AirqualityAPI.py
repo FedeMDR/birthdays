08c8b7bdc219ddf03370781cd122a3526e4e60fb
TrivialHarp16 <simonedilorenzo00@gmail.com>
2023-11-28 17:02:36 +0100

    pusshingp
    ce l'abbiamo fatta un API

--- AirqualityAPI.py
+++ AirqualityAPI.py
@@ -1,1 +1,11 @@
-test = http://api.openweathermap.org/data/2.5/air_pollution?lat=40.68457&lon=-73.91181&appid=596dff3ac05aeb906e63803d2bfcf01a
+import json
+import requests
+
+
+test = "http://api.openweathermap.org/data/2.5/air_pollution?lat=40.68457&lon=-73.91181&appid=596dff3ac05aeb906e63803d2bfcf01a"
+
+response = requests.get(test)
+
+json_data = response.json()
+
+print(json_data)
--- Colonne.py
+++ Colonne.py
@@ -2,4 +2,3 @@ usefulcol_Airbnb = ["listing_url", "name", "description", "picture_url",
                         "host_url","neighbourhood_cleansed", "neighbourhood_group_cleansed",
                             "latitude", "longitude","price", "review_scores_rating"]
 
-vaccadio
